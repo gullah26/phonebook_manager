a879b3c9d4e7b8f66ff121b964f1953bdc84171d
Seun Ajetomobi <solaajetomobi@aol.com>
2022-04-03 10:30:45 +0000

    Initial commit and title functon

---
+++ shoppingCart.py
@@ -0,0 +1,21 @@
+import sys  # module exit the program when called
+
+
+# create a title function
+def title():
+    print("\n                        $$$$$$$$$$$$$")
+    print("                                 $$$$")
+    print("          SHOPPING CART  V1.0    $$$$")
+    print("                                 $$$$")
+    print("         $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+
+title()
+
+name = input("\n           Enter your name: ")
+print("\n      ---------------------------------------------")
+print(f"      Hello {name}, welcome to your shopping cart ")
+print("      ---------------------------------------------")
+print("\n       <<< Press Enter to access menu >>>    ")
+input()
+

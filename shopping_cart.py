f10d78c8d1a65d2bee2b2ce246eeb9d3cfa5d348
Seun Ajetomobi <solaajetomobi@aol.com>
2022-04-04 11:35:26 +0000

    Add empty cart function and debug view function

--- shopping_cart.py
+++ shopping_cart.py
@@ -3,7 +3,7 @@ import sys
 
 
 # create a title function
-def title():
+def app_name():
     print("""\n                         $$$$$$$$$$$$$""")
     print("""                                 $$$$""")
     print("""        Shopping Cart  V1.0     $$$$""")
@@ -11,9 +11,9 @@ def title():
     print("""       $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$""")
 
 
-title()
+app_name()
 
-name = input("\n           Enter your name: ")
+name = input("\n           Enter your name: ").title()
 print("\n      ---------------------------------------------")
 print(f"      Hello {name}, welcome to your shopping cart ")
 print("      ---------------------------------------------")
@@ -27,9 +27,9 @@ myCart = []
 def dash_board():
     while True:
         print("""  <<<<<<<< OPTION MENU >>>>>>>> """)
-        print("""  ****************************""")
+        print("""  """)
         print("""  Press 1 - 6 to select option""")
-        print("""  ****************************""")
+        print("""  """)
         print("     1: Add item")
         print("     2: View cart")
         print("     3: Search item")
@@ -68,7 +68,7 @@ def dash_board():
             print("     =========================""\n")
             print("Press Enter to Exit")
             input()
-            sys.exit(" **** Thank you for your time ***")
+            sys.exit(" --( Thank you for your time =:)--")
         else:
             print("Invalid option selected, RETRY!!!")
             print("Press enter to Return to Menu")
@@ -77,9 +77,9 @@ def dash_board():
 
 # function to Add items to the shopping cart
 def add_item():
-    item = input("\nAdd item to cart: ")
-    myCart.append(item.title())
-    print()
+    item = input("\nAdd item to cart: ").title()
+    myCart.append(item)
+    print("\n")
     print(f"____{item} added to cart____""\n")
     print("\nPress Enter to return to Menu")
     input(".......")
@@ -87,27 +87,28 @@ def add_item():
 
 # displays the list of items in the cart
 def view_cart():
-    print()
+    print("\n")
     print(f"   Total item(s) in your cart is : {len(myCart)} ""\n")
-    for item in myCart:
-        print(item)
-        break
+    if len(myCart) > 0:
+        for i, item in enumerate(myCart, 1):
+            print(f"   item {i}: {item}")
     else:
         print("\n____!!!Your shopping cart is empty____""\n")
-        print("\nPress Enter to return to Menu")
-        input(".......")
+    print("\nPress Enter to return to Menu")
+    input(".......")
 
 
 # Find item in the cart based on user input
 def search_item():
-    item = input("Find item in cart: ")
+    item = input("Find item in cart: ").title()
     if item in myCart:
-        print()
+        print("\n")
         print(f"____{item} is in cart____""\n")
         print("\nPress Enter to return to Menu")
         input(".......")
         dash_board()
     else:
+        print("\n")
         print(f"____{item} is not in cart____""\n")
         print("\nPress Enter to return to menu")
         input(".......")
@@ -115,7 +116,7 @@ def search_item():
 
 # Finds and delete an item from the cart based on user input
 def delete_item():
-    item = input("Find item to delete from cart: ")
+    item = input("Find item to delete from cart: ").title()
     if item in myCart:
         myCart.remove(item)
         print(f"\n____{item} deleted from cart____""\n")
@@ -130,19 +131,23 @@ def delete_item():
 
 # Empty the cart
 def empty_cart():
-    print("\nThis will erase all items in the cart")
+    print("\n### This will erase all items in the cart ###""\n")
     answer = input("\nPress y to erase cart or n to return to menu: ")
-    if answer == "y":
-        print("\n____All item(s) in cart deleted____""\n")
-        print("Press enter to return to Menu")
-        input(".......")
-        dash_board()
+    if len(myCart) > 0:
+        if answer == "y":
+            myCart.clear()
+            print("\n### Cart is now empty ###""\n")
+            print("Press enter to return to Menu")
+            input("...")
+            dash_board()
     elif answer == "n":
-        print("Press enter to return to Menu")
+        print("\nPress enter to return to Menu""\n")
+        input("...")
         dash_board()
     else:
-        print("\nPress enter to return to Menu")
-        input("........")
+        print("\n### Nothing to Erase in cart ###")
+        print("\nPress enter to return to Menu""\n")
+        input("......")
         dash_board()
 
 

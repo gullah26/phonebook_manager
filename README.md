c7dc5642a59ad3794e21d4dd53703138e3016e2b
Seun Ajetomobi <solaajetomobi@aol.com>
2022-04-08 01:27:03 +0000

    Final check

--- README.md
+++ README.md
@@ -1,45 +1,189 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# Shopping List Manager #
 
-Welcome Seun Ajetomobi,
+* The shopping cart manager is an direct equivalent to the popular shopping list manager program, It is a kind of shopping bot that allows the user input and manipulation of data according to the user choices and the shopping bot outputs a result based on user choices and input.
 
-This is the Code Institute student template for Gitpod. We have preinstalled all of the tools you need to get started. You can safely delete this README.md file, or change it for your own project. Please do read it at least once, though! It contains some important information about Gitpod and the extensions we use.
+* The aim of this program is to help user have a proper planning with grocery shopping, by having a grocery list is beneficial to a user in a lot of ways.
+    First, a user will likely make an improptu decision to impulse buy a product that was never in the plan, this kind of uncontrolled urge will definitly impact the user financially, which the ripple effect can spread all the way to incur debt , which can eventually be a detriment to a once healthy credit score. However, having a proper planning of the items you want to buy could come in handy as it will help in avoiding over spending, and impulse buying by helping user to focus on what is really needed.
 
-## Gitpod Reminders
+* Secondly, the shopping list program saves user time and energy, not having a list of what to buy at the store makings shopping a headache and time consuming as wider product choices can be a distraction which can be overwhelming sometimes, but if you already have the choice of items you which to buy makes shopping very easy and less time consuming. In the same vein, having a shopping list reduces waste in the sense that when you buy things you do not really need immediatly it most times end up in the trash.
 
-To run a frontend (HTML, CSS, Javascript only) application in Gitpod, in the terminal, type:
+* Further more, it can help in improving user memory and helps user to focus and have proper planning of things.
 
-`python3 -m http.server`
+![title and logo screen](/images/title.png)
 
-A blue button should appear to click: *Make Public*,
+## Features
+_______________________________
 
-Another blue button should appear to click: *Open Browser*.
+### Greeting
 
-To run a backend Python file, type `python3 app.py`, if your Python file is named `app.py` of course.
+*  The first thing a user sees when the program is been executed is a display of the program title with a design.
 
-A blue button should appear to click: *Make Public*,
+* It asks the user to input a name which is then displayed as a greeting.
 
-Another blue button should appear to click: *Open Browser*.
 
-In Gitpod you have superuser security privileges by default. Therefore you do not need to use the `sudo` (superuser do) command in the bash terminal in any of the lessons.
+![greeting](/images/greetings.png)
 
-## Updates Since The Instructional Video
 
-We continually tweak and adjust this template to help give you the best experience. Here is the version history:
+### Option Menu 
+* This is a section the shows  to the user available option to selct from. 
 
-**October 21 2020:** Versions of the HTMLHint, Prettier, Bootstrap4 CDN and Auto Close extensions updated. The Python extension needs to stay the same version for now.
+* This option ranges from 1 to 7.
 
-**October 08 2020:** Additional large Gitpod files (`core.mongo*` and `core.python*`) are now hidden in the Explorer, and have been added to the `.gitignore` by default.
+* Based on user selection the program execute each task accordingly.
+	
 
-**September 22 2020:** Gitpod occasionally creates large `core.Microsoft` files. These are now hidden in the Explorer. A `.gitignore` file has been created to make sure these files will not be committed, along with other common files.
 
-**April 16 2020:** The template now automatically installs MySQL instead of relying on the Gitpod MySQL image. The message about a Python linter not being installed has been dealt with, and the set-up files are now hidden in the Gitpod file explorer.
+![Option Menu](/images/optionmenu.png)
 
-**April 13 2020:** Added the _Prettier_ code beautifier extension instead of the code formatter built-in to Gitpod.
+#### Option 1: Add item to list.
 
-**February 2020:** The initialisation files now _do not_ auto-delete. They will remain in your project. You can safely ignore them. They just make sure that your workspace is configured correctly each time you open it. It will also prevent the Gitpod configuration popup from appearing.
+* This option allow user to add items to the list.
+* It also tells the user if an item has already been added to avoid repetition.
 
-**December 2019:** Added Eventyret's Bootstrap 4 extension. Type `!bscdn` in a HTML file to add the Bootstrap boilerplate. Check out the <a href="https://github.com/Eventyret/vscode-bcdn" target="_blank">README.md file at the official repo</a> for more options.
 
---------
+![object selection section](assets/images/optionarea.png)
+![object selection section](assets/images/optionarea.png)
 
-Happy coding!
+#### Option 2: View List
+
+* This option displays the content of the list. 
+
+* If nothing was previously added it displays "List is empty, try adding adding an item".
+
+![Footer section](assets/images/gameplay.png)
+![Footer section](assets/images/gameplay.png)
+
+
+#### Option 3: Total number of items in the list
+
+* This option displays the total number of items on the list. 
+
+* Displays "0" if the list is empty
+
+
+![score section](assets/images/playerscore.png)
+
+#### Option 4: Find items in the list
+	
+* The find item in the list option searches the list for a user specified item name, which then search and returns a message if the item is found or not 
+
+
+![Footer section](assets/images/rules.png)
+![Footer section](assets/images/rules.png)
+
+
+#### Option 5: Delete items from the list
+
+* The delete item from the list option also searches the list for a user specified item name, which then search and delete the item if found on the list or not.
+
+
+![Footer section](assets/images/rules.png)
+![Footer section](assets/images/rules.png)
+
+
+#### Option 6: Empty  the list
+
+* This erase every content of the list and return it back to an empty list.
+* It prints a message based on if the list is already empty or not.
+
+![Footer section](assets/images/rules.png)
+![Footer section](assets/images/rules.png)
+
+#### Option 7: Exit program
+
+* This function allows user to exit the program.
+* It gives the user an option to chose either to exit or run the program again from the beginning.
+* It also print a thank you message to the user based on the time of the day.
+
+![Footer section](assets/images/rules.png)
+
+## Testing
+
+* This program was properly tested using Pep8 online [Pep8](https://www.http://pep8online.com/checkresult "pep8") .
+
+* All the generated errors like "trailing white spaces and no newline at the end of file were rectified.
+
+* I confirm that all the functionality of this program works perfectly.
+
+![Footer section](/images/pep8.png)
+## Tools used
+
+### Language
+* Python
+
+### Other program used: 
+* Gitpod:
+    * Gitpod: A versatile IDE used to code the program [Gitpod](https://www.gitpod.io/ "gitpod")
+
+* Github:
+    * Github: a platform used for storing, tracking, and collaborating on software projects  [Github](https://www.github.com/ "github")
+
+* Git:
+    * Git: Used for version control.
+
+* Pep8:
+    * pep8: an online python code checker  [pep8](https://http://pep8online.com/checkresult/ "pep8")
+
+* Heroku:
+    * Heroku: used in the deployement of the program  [Heroku](https://http://heroku.com/ "Heroku")
+
+
+
+## Bugs
+* No bugs was found as at the time of deployment.
+
+
+##  User Input Validation.
+
+* User input validation
+	* The program code was written in a way that it controls any invalid user data input, It allows inputs like "Peanut butter", "Scottish- Whiskey". The error messages were controlled by use of various conditional statements.
+
+## Deployment
+
+* The program was deployed to Heroku by following this steps.
+
+* On the Heroku wesbite, create an account.
+
+* After signing in into your account click on create new app button.
+
+* Chose a unique name for the application to be deployed.
+
+* Chose a region.
+
+* Click on settings.
+
+* Navigate to Config var.
+
+* In the Key Value space we need to input PORT for KEY must be in capitals and 800 for PORT
+
+* Click add.
+
+* Click add buildpack, choose python first save changes and add nodjs must be in this order.
+
+* Navigate to the deploy tab on the menu.
+
+* Chose your choice of deployment github is the choice for this project.
+
+* Enter your github repository name and search, if found then click connect.
+
+* Chose to either deploy automatically or manually.
+
+* On this project, our choice is manual deployment, click and wait until a message appear confirming that the application was successfully deployed.
+
+* Click on view app button to run the application.
+
+
+* The Live link to the deployed program is  [Shopping list](https://gullah26.github.io/Rock_Paper_Scissors/ "rock paper scissors").
+
+## Credits
+
+* Content
+
+	* The part of the code to make the function check winner was from  [sebhastian](https://sebhastian.com/rock-paper-scissors-javascript/ "sebhastian")
+
+	* The inspiration to start  the project is from  Code Institute Portfolio Project Scope.
+
+
+## Media
+
+* The Images used on this game  was taken from [png set](https://pngset.com/download-free-png-yiceg "png set").
--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import sys
 from datetime import datetime
 
 # An empty list that will contain user input
-my_cart = []
-new_cart = []
+my_list = []
+new_list = []
 
 
 def greeting():
@@ -11,7 +11,7 @@ def greeting():
     # The program title logo
     print("\n      %%%%%%%%%              %%%%%%%%%")
     print("      %%%                          %%%")
-    print("      %%%    Shopping Cart  V1.0   %%%")
+    print("      %%%    Shopping list  V1.0   %%%")
     print("      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 # welcome message that greets the user based on time of the day
     while True:
@@ -21,17 +21,17 @@ def greeting():
         elif not name:
             print("\nPlease enter a valid input\n")
         elif this_hour < 12:
-            print(f"\nGood morning {name}, welcome to your shopping")
-            print("=--------------------------------------------------------=")
+            print(f"\nGood morning {name}, welcome to your shopping list")
+            print("=-----------------------------------------------------=")
             input("\n<<< Press Enter to access menu:>>>\n")
         elif 12 <= this_hour < 18:
-            print(f"\nGood afternoon {name}, welcome to your shopping cart")
-            print("=--------------------------------------------------------=")
+            print(f"\nGood afternoon {name}, welcome to your shopping list")
+            print("=------------------------------------------------------=")
             input("\n<<< Press Enter to access menu:>>>\n")
             continue
         else:
-            print(f"\nGood evening {name}, welcome to your shopping cart")
-            print("=--------------------------------------------------------=")
+            print(f"\nGood evening {name}, welcome to your shopping list")
+            print("=----------------------------------------------------=")
             input("\n<<< Press Enter to access menu:>>>\n")
             break
 
@@ -45,24 +45,24 @@ def option_menu():
         print("\nPress 1 - 7 to select option")
         print("  ")
         print("1: Add item")
-        print("2: View cart")
-        print("3: Total item in cart")
+        print("2: View list")
+        print("3: Total item in list")
         print("4: Find item")
         print("5: Delete item")
-        print("6: Empty cart  ")
+        print("6: Empty list  ")
         print("7: Exit app ""\n")
         # User input to select an option
         option = input("Select option:\n")
         if option == "1":
-            print("<<  Add item to Cart  >>")
+            print("<<  Add item to list  >>")
             print("=========================""\n")
             add_item()
         elif option == "2":
-            print("   <<  View cart  >>")
+            print("   <<  View list  >>")
             print("=======================""\n")
-            view_cart()
+            view_list()
         elif option == "3":
-            print("  << Number of Items in cart >>")
+            print("  << Number of items in list >>")
             print("================================""\n")
             item_total()
         elif option == "4":
@@ -74,13 +74,13 @@ def option_menu():
             print("======================""\n")
             delete_item()
         elif option == "6":
-            print("   <<  Empty cart >>")
+            print("   <<  Empty list >>")
             print("======================""\n")
-            empty_cart()
+            empty_list()
         elif option == "7":
             print("  <<  Exit program >>")
             print("=======================""\n")
-            exit_app()
+            exit_program()
         elif not option:
             print("** Invalid input please enter 1-7 **")
             input("\nPress enter to return to Menu\n")
@@ -92,7 +92,7 @@ def option_menu():
 def add_item():
     while True:
         item_add = (
-            input("\nAdd item to cart:\n")
+            input("\nAdd item to list:\n")
             .title()
             .strip()
         )
@@ -104,60 +104,60 @@ def add_item():
         elif not item_add:
             print("\nPlease enter a valid input\n")
             continue
-        elif item_add + "\n" in my_cart:
-            print(f"\n{item_add} was previously added to cart""\n")
+        elif item_add + "\n" in my_list:
+            print(f"\n{item_add} was previously added to list""\n")
             input("\nPress enter to continue\n")
             break
         else:
-            print(f"\n \"{item_add}\" was added to cart\n")
+            print(f"\n \"{item_add}\" was added to list\n")
             input("\nPress enter to return to Menu\n")
-            my_cart.append(item_add + "\n")
+            my_list.append(item_add + "\n")
             save_data()
             break
 
 
-# The content of the shopping cart is displayed by calling this function
-def view_cart():
-    if len(my_cart) > 0:
+# The content of the shopping list is displayed by calling this function
+def view_list():
+    if len(my_list) > 0:
         with open("cart_data.txt", "r+", encoding='utf-8'):
-            for i, item in enumerate(my_cart, 1):
+            for i, item in enumerate(my_list, 1):
                 print(f"   item {i}: {item}", end="")
             input("\nPress Enter to continue\n")
     else:
-        print("\nCart is empty, Try adding an item\n")
+        print("\nlist is empty, Try adding an item\n")
         input("\nPress enter to return to Menu\n")
 
 
 def item_total():
-    print(f"\nTotal number of item(s) in your cart is : {len(my_cart)}\n")
+    print(f"\nTotal number of item(s) in your list is : {len(my_list)}\n")
     input("\nPress Enter to continue\n")
 
 
 # This function opens the file to read from the saved data
 def get_cart_data():
-    with open("cart_data.txt", "r", encoding='utf-8') as my_list:
-        for line in my_list:
-            my_cart.append(line)
+    with open("cart_data.txt", "r", encoding='utf-8') as file:
+        for line in file:
+            my_list.append(line)
 
 
 # Save data in "W" write mode overwrites and clear any existing data
 def save_data():
-    cart_list = new_cart + my_cart
+    list_list = new_list + my_list
     with open("cart_data.txt", "w", encoding='utf-8') as data_file:
-        for x in cart_list:
+        for x in list_list:
             if x != "\n":
                 data_file.write(x)
 
-    new_cart.clear()
-    my_cart.clear()
+    new_list.clear()
+    my_list.clear()
     get_cart_data()
 
 
-# Find item in the cart based on user input and return a message
+# Find item in the list based on user input and return a message
 def find_item():
     while True:
         search_item = (
-            input("Find item in cart:\n")
+            input("Find item in list:\n")
             .title()
             .strip()
         )
@@ -167,23 +167,23 @@ def find_item():
         # elif any(not char.isalnum() for char in search_item):
         #     print("\n### Enter a valid input ###\n")
         #   continue
-        elif search_item + '\n' in my_cart:
-            print(f"\n \"{search_item}\" is in your cart\n")
+        elif search_item + '\n' in my_list:
+            print(f"\n \"{search_item}\" is in your list\n")
             input("\nPress Enter to continue:\n")
             break
         elif not search_item:
             print("\n### Enter a valid input ###\n")
         else:
-            print(f"\n \"{search_item}\" is not in your cart\n")
+            print(f"\n \"{search_item}\" is not in your list\n")
             input("\nPress Enter to continue:\n")
             break
 
 
-#  This particular function finds and delete a specific item from the cart
+#  This particular function finds and delete a specific item from the list
 def delete_item():
     while True:
         to_delete = (
-            input("\nFind item to delete from cart:\n")
+            input("\nFind item to delete from list:\n")
             .title()
             .strip()
             )
@@ -195,39 +195,39 @@ def delete_item():
         #     continue
         elif not to_delete:
             print("\nEnter a valid input\n")
-        elif to_delete + '\n' not in my_cart:
-            print(f"\n!!!Oops, \"{to_delete}\" is NOT in your cart")
+        elif to_delete + '\n' not in my_list:
+            print(f"\n\"{to_delete}\" not deleted , it is NOT in your list")
             input("\nPress enter to continue:\n")
             break
         else:
-            my_cart.remove(to_delete + "\n")
-            print(f"\n \"{to_delete}\" deleted from your cart\n")
+            my_list.remove(to_delete + "\n")
+            print(f"\n \"{to_delete}\" found and deleted from your list\n")
             save_data()
             input("\nPress Enter to continue:\n")
             break
 
 
-# When this function is called, it empties the cart
-def empty_cart():
+# When this function is called, it empties the list
+def empty_list():
     while True:
-        if len(my_cart) > 0:
-            my_cart.clear()
-            print("\nYour shopping cart is now empty\n")
+        if len(my_list) > 0:
+            my_list.clear()
+            print("\nYour  list is now empty\n")
             save_data()
             input("\nPress enter to return to Menu:\n")
             option_menu()
             break
         else:
-            print("\nShopping cart is already empty")
+            print("\nList is already empty, nothing to erase")
             input("\nPress enter to return to Menu:\n")
             option_menu()
             break
 
 
 # This Exit function terminates  the program execution
-def exit_app():
+def exit_program():
     exit_hour = int(datetime.now().strftime('%H'))
-    choice = input("\nPress y to exit or n to re-start program\n")
+    choice = input("\nPress \"y\" to exit or \"n\" to re-start program\n")
     if choice == "y":
         if exit_hour < 12:
             print("\nThank you for your time =:)\n")
